d980ca3967d2d84c456fb55bd2cf65a8fe1b52a0
Talha Imran <timran@psu.edu>
2021-02-15 21:39:05 -0700

    Updates for infra

---
+++ scripts/start_vhive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+set -x
+
+sudo unlink /etc/firecracker-containerd/fccd-cri.sock
+sudo ${SCRIPT_DIR}/clean_fcctr.sh
+
+sudo containerd &
+sleep 10
+sudo PATH=${PATH} /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml &
+sleep 15
+
+cd ${SCRIPT_DIR}/../
+source /etc/profile && go build && sudo ./vhive
+
+set +x
