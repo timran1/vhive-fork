ab902c4237d756bc1a9aa06e7171979d9207f8b3
Talha Imran <timran@psu.edu>
2021-02-15 20:45:24 -0700

    Do not install k8s

--- scripts/install_stock.sh
+++ scripts/install_stock.sh
@@ -40,16 +40,16 @@ containerd --version || echo "failed to build containerd"
 
 
 # Install k8s
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list"
-sudo apt update >> /dev/null
-sudo apt -y install cri-tools ebtables ethtool kubeadm kubectl kubelet kubernetes-cni
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list"
+# sudo apt update >> /dev/null
+# sudo apt -y install cri-tools ebtables ethtool kubeadm kubectl kubelet kubernetes-cni
 
 # Install knative CLI
-git clone https://github.com/knative/client.git $HOME/client
-cd $HOME/client
-hack/build.sh -f
-sudo mv kn /usr/local/bin
+# git clone https://github.com/knative/client.git $HOME/client
+# cd $HOME/client
+# hack/build.sh -f
+# sudo mv kn /usr/local/bin
 
 
 # Necessary for containerd as container runtime but not docker
