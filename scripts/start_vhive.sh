d0c062458b5ae2a32c1372d45192f6751eabac28
Talha Imran <timran@psu.edu>
2021-02-24 16:22:58 -0700

    Fix logs and start script

--- scripts/start_vhive.sh
+++ scripts/start_vhive.sh
@@ -7,12 +7,13 @@ set -x
 sudo unlink /etc/firecracker-containerd/fccd-cri.sock
 sudo ${SCRIPT_DIR}/clean_fcctr.sh
 
-sudo containerd &
+cd ${SCRIPT_DIR}/../
+
+sudo containerd &> ./containerd.log &
 sleep 10
 sudo PATH=${PATH} /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml &
 sleep 15
 
-cd ${SCRIPT_DIR}/../
-source /etc/profile && go clean && go build && sudo ./vhive --dbg |& tee ~/disagg-mem-sys/vhive.log
+source /etc/profile && go clean && go build && sudo ./vhive &> ./vhive.log
 
 set +x
