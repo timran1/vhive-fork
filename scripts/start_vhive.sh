66d5fee9beb77d9358915832fc915c440cffaa0f
Talha Imran <timran@psu.edu>
2021-02-25 18:12:38 -0700

    Change mem limit

--- scripts/start_vhive.sh
+++ scripts/start_vhive.sh
@@ -9,11 +9,13 @@ sudo ${SCRIPT_DIR}/clean_fcctr.sh
 
 cd ${SCRIPT_DIR}/../
 
+rm vhive.log containerd.log fc-containerd.log
+
 sudo containerd &> ./containerd.log &
 sleep 10
 sudo PATH=${PATH} /usr/local/bin/firecracker-containerd --config /etc/firecracker-containerd/config.toml &> ./fc-containerd.log &
 sleep 15
 
-source /etc/profile && go clean && go build && sudo ./vhive &> ./vhive.log
+source /etc/profile && go clean && go build && sudo ./vhive --hostIface ${HOST_INTERFACE} &> ./vhive.log
 
 set +x
